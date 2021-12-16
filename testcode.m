5c396d9beec6a9168f29382607e251b082b381f9
ZHIBIN ZHOU <zhibinz2@uci.edu>
2021-12-15 16:43:22 -0800

    init

---
+++ testcode.m
@@ -0,0 +1,46 @@
+cd C:\Users\NIRS\Desktop\USB documents\Matlab\matlab file converter
+addpath Matlab
+
+oxysoft2matlab
+
+oxysoft2matlab ({'20211214.oxy5', 'Measurement Examples.oxyproj'})
+
+%%
+DataFNIR=ans;
+RighthandData=ans;
+LefthandData=ans;
+
+oxyvals=DataFNIR.oxyvals;
+dxyvals=DataFNIR.dxyvals;
+time=DataFNIR.time;
+SampleNo=DataFNIR.sampleNo;
+wavelengths=DataFNIR.wavelengths;
+DPF=DataFNIR.DPF;
+diatance=DataFNIR.distance;
+Fs=DataFNIR.Fs;
+label=DataFNIR.label;
+RxLabel=DataFNIR.RxLabel;
+TxLabel=DataFNIR.TxLabel;
+Rx_TxId=DataFNIR.Rx_TxId;
+chanPos=DataFNIR.chanPos;
+transPos=DataFNIR.transPos;
+receiPos=DataFNIR.receiPos;
+ADlabel=DataFNIR.ADlabel;
+ADvalues=DataFNIR.ADvalues;
+events=DataFNIR.events;
+%%
+oxyvalsR=DataFNIR.oxyvals;
+dxyvalsR=DataFNIR.dxyvals;
+timeR=DataFNIR.time;
+
+oxyvalsL=DataFNIR.oxyvals;
+dxyvalsL=DataFNIR.dxyvals;
+timeL=DataFNIR.time;
+
+figure;
+subplot(2,1,1);
+plot(timeR,oxyvalsR-repmat(mean(oxyvalsR(1:50,:),1),2766,1),'r'); hold on;
+plot(timeR,dxyvalsR-repmat(mean(dxyvalsR(1:50,:),1),2766,1),'b'); hold off;
+subplot(2,1,2);
+plot(timeL,oxyvalsL-repmat(mean(oxyvalsL(1:50,:),1),3216,1),'r'); hold on;
+plot(timeL,dxyvalsL-repmat(mean(dxyvalsL(1:50,:),1),3216,1),'b'); hold off;
